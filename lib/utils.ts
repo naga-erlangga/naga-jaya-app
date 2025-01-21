d1fc3cb936ddd4e3f45a108f82524355b04a5125
Naga Jaya <erlangga.naga.jaya.42@gmail.com>
2025-01-21 16:30:20 +0700

    Updated new Search
    Search now will find better and based on word only.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,24 +1,41 @@
-import {type ClassValue, clsx} from "clsx"
-import {twMerge} from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 import fuzzysearch from "fuzzysearch-ts";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export const currencyFormat = new Intl.NumberFormat('id-ID', {
-  style: 'currency',
-  currency: 'IDR',
+export const currencyFormat = new Intl.NumberFormat("id-ID", {
+  style: "currency",
+  currency: "IDR",
 });
 
-export const fuzzySearch = (needle : string, haystack : any[]) => {
+const checkText = (needle: string, haystack: string): boolean => {
+  // in haystack check if needle in haystack
+  let wordInHaystack = haystack
+    .split(" ")
+    .map((e) => e.toLowerCase())
+    .includes(needle);
+
+  // debug purposes
+  // if (wordInHaystack) {
+  //   console.log(haystack.split(" "));
+  //   console.log(wordInHaystack);
+  // }
+
+  // if word in haystack and fuzzy search also agree with the value
+  return fuzzysearch(needle, haystack.toLowerCase()) && wordInHaystack;
+};
+
+export const fuzzySearch = (needle: string, haystack: any[]) => {
   let filtered = [];
 
   for (let i = 0; i < haystack.length; i++) {
-    if (fuzzysearch(needle, haystack[i].name.toLowerCase())) {
-      filtered.push(haystack[i])
+    if (checkText(needle, haystack[i].name)) {
+      filtered.push(haystack[i]);
     }
   }
 
   return filtered;
-}
+};
